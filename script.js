e784eda60ac9f8af6923c998efb4afe7e9c17888
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-06-06 18:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    event.preventDefault(); // Prevent the form from submitting in the traditional way
 
+    // Get the form elements by their names
+    const form = document.getElementById('form1');
+    const fname = form.elements['fname'].value;
+    const lname = form.elements['lname'].value;
+
+    // Display the values using an alert
+    alert(`${fname} ${lname}`);
 }
+
+// Attach the event listener to the form
+document.getElementById('form1').addEventListener('submit', getFormvalue);
+
